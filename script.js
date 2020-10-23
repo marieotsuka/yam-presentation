43bb3cd9f4b79a2b350dddccd40aa30b90f81eb1
marieotsuka <marieotsuka@users.noreply.github.com>
2020-10-23 16:30:57 -0400

    add image assets

---
+++ script.js
@@ -0,0 +1,23 @@
+var searchoptions = {
+  strings: [
+  'Free photoshop alternatives',
+  'How to antialias with MS Paint',
+  'Illustrator vs. Photoshop vs. InDesign',
+  'Free coding tutorials',
+  'What is a server?',
+  'What is the cloud?',
+  'HTML tutorial',
+  'Text Editor download',
+  'lisa explains it all',
+  'CSS Layouts',
+  'Free hosting services',
+  'Absolute vs. relative paths',
+  'Raster vs. vector image formats',
+  'JavaScript introduction',
+  'jQuery tutorial'
+  ],
+  typeSpeed: 50,
+  loop: true,
+};
+
+var typed = new Typed('#searchbar', searchoptions);
